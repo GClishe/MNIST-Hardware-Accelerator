04b3648f019816d45f5019b36521c1cecb467c39
GClishe <clishegavin@gmail.com>
2026-03-18 18:17:41 -0400

    Added state machine states

--- sources/design/Control_Unit.sv
+++ sources/design/Control_Unit.sv
@@ -3,7 +3,8 @@
 module Control_Unit # (
     parameter int ACT_W = 8,      // size of the activations
     parameter int WGT_W = 8,      // size of the weights
-    parameter int BIAS_W = 8      // size of the bias
+    parameter int BIAS_W = 8,      // size of the bias
+    parameter int NUM_PE = 4
 
 ) (
     input clk,
@@ -17,11 +18,18 @@ module Control_Unit # (
     output APPLY_ACT,   // applies RELU and clamps accumulator output (after biasing) to 8 bits unsigned. 
 );
 
-typedef enum logic [2:0] {          // defines a named type `state`, encoded in 3 bits
-        S_IDLE  = 3'd0,             // waiting for start signal
-        S_MAC  = 3'd1,              // Enabling PE MAC units and fetching weights and activations from memory
-        S_ACTIVATE = 3'd2           // Applying activation function
-        S_STORE  = 3'd3             // Writing results back to memory or outputs
+typedef enum logic [3:0] {          // defines a named type `state`, encoded in 4 bits
+        S_START         = 4'd0,     // start state. begin here
+        S_CLEAR         = 4'd1,     // broadcasts reset signal to PEs. Also resets all counters 
+        S_IDLE          = 4'd2,     // Select layer index 0 so that LOAD_MEM will load input activations. When input activations done loading in memory, advance to LOAD_MEM
+        S_LOAD_MEM      = 4'd3,     // activations from layer index are loaded to registers near process engines, and biases are loaded to the back of these registers
+        S_MAC           = 4'd4,     // MAC operation commences
+        S_BIAS          = 4'd5,     // BIAS operation
+        S_ACTIVATE      = 4'd6,     // actiavtion operation
+        S_STORE         = 4'd7,     // activations stored to memory at layer index i+1. Parallel -> serial conversion so that activations stored in correct order
+        S_ADVANCE_TILE  = 4'd8,     // if the activations stored in layer i+1 is less than the number expected, advance the tile (do NOT increment layer index) and proceed to LOAD_MEM
+        S_ADVANCE_LAYER = 4'd9,     // if activations store in layer i+1 is equal to number expected, advance layer (increment layer index) and proceed to LOAD_MEM
+        S_BROADCAST     = 4'd10     
     } state_t;
 
 state_t state;  // declaring state variable with type state_t
