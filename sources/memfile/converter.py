6dbcd66766b219515b49d42a1414bd2f8ed8221a
GClishe <clishegavin@gmail.com>
2026-04-22 13:16:22 -0400

    Added file

--- sources/memfile/converter.py
+++ sources/memfile/converter.py
@@ -1,5 +1,5 @@
-input_file = "testImage_0.mem"
-output_file = "testImage_0_b.mem"
+input_file = "testImage_1.mem"
+output_file = "testImage_1_b.mem"
 
 with open(input_file, "r") as fin, open(output_file, "w") as fout:
     for line in fin:
