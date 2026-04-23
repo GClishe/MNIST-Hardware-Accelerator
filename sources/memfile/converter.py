79a659162f419cf137f7d8f272603d5c574bccd1
GClishe <clishegavin@gmail.com>
2026-04-23 00:31:54 -0400

    Altered input file conversion

--- sources/memfile/converter.py
+++ sources/memfile/converter.py
@@ -1,11 +1,12 @@
-input_file = "testImage_9.mem"
-output_file = "testImage_9_b.mem"
+for i in [0,1,2,3,4,5,6,7,8,9]:
+    input_file = f"testImage_{i}.mem"
+    output_file = f"testImage_{i}_b.mem"
 
-with open(input_file, "r") as fin, open(output_file, "w") as fout:
-    for line in fin:
-        content = line.rstrip("\n")
-        
-        if content == "FF":
-            fout.write("01\n")
-        else:
-            fout.write(line)
+    with open(input_file, "r") as fin, open(output_file, "w") as fout:
+        for line in fin:
+            content = line.rstrip("\n")
+            
+            if content == "FF":
+                fout.write("100000000\n")
+            else:
+                fout.write("000000000\n")
